ea3003896f20ec2a01800ba478ad8b96c6920f04
VSartz <elsartz@gmail.com>
2022-07-25 11:58:47 -0400

    merged

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,12 +1,10 @@
-import './footer.module.css';
+import React from 'react'
 
-export default function Footer () {
-    return <Footer className='footer'>
-   
-    </Footer>
+export default function Footer() {
+  return (
+    <>
+   <h2>This is a footer</h2>
+    </>
 
+  )
 }
-
-
-
-
