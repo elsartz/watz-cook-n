cc700ac1601474fde3ba0a638c96e659550e9110
VSartz <elsartz@gmail.com>
2022-07-25 12:12:32 -0400

    correct paths for Navigation and footer

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ import {
 import { setContext } from '@apollo/client/link/context';
 
 import NoMatch from './pages/NoMatch';
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer';
+import Navbar from './components/nav/Navbar'
+import Footer from './components/footer/Footer';
 
 const httpLink = createHttpLink({
     uri: '/graphql',
