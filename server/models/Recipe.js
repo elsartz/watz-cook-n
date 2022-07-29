a737bf66616d2c9932ae87ac1043f6e377bd8741
VSartz <elsartz@gmail.com>
2022-07-29 11:24:16 -0400

    copy working folder part two

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -2,29 +2,45 @@ const { Schema } = require('mongoose');
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedRecipes` array in User.js
 const recipeSchema = new Schema({
-  authors: [
-    {
-      type: String,
-    },
-  ],
-  description: {
-    type: String,
-    required: true,
-  },
   // saved recipe id
   recipeId: {
     type: String,
     required: true,
   },
-  image: {
-    type: String,
-  },
-  link: {
-    type: String,
-  },
   title: {
     type: String,
     required: true,
+  },
+  cuisines: {
+    type: String,
+    required: true,
+  },
+  extendedIngredients: {
+    type: String,
+    required: true,
+  },
+  summary: {
+    type: String,
+    required: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  vegetarian: {
+    type: Boolean,
+  },
+  glutenFree:{
+    type: Boolean
+  },
+  healthScore: {
+   type: Number
+  },
+  image: {
+    type: String,
+  },
+  sourceUrl: {
+    type: String,
   },
 });
 
