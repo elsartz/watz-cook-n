6982661866e5b6b60320000493a0ed13f67094ff
VSartz <elsartz@gmail.com>
2022-07-31 11:16:00 -0400

    footer fix

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import {Fstyle} from './Footer.style';
 export default function Footer() {
   return (
     <div style={{background:"#5000ca"}}>
-      <Fstyle/>
+
       <p style={{textAlign:"center"}}>Copyright 2022 by minions</p>
     </div>
 
