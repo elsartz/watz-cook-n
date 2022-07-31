33be72d720abb677c9a20f91e2715146df08c1cc
VSartz <elsartz@gmail.com>
2022-07-31 11:24:55 -0400

    fooTer unKnown

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import {Fstyle} from './Footer.style';
+import React from 'react'
 
 export default function Footer() {
   return (
-    <div style={{background:"#5000ca"}}>
-
-      <p style={{textAlign:"center"}}>Copyright 2022 by minions</p>
-    </div>
+    <>
+   <h2>This is a footer</h2>
+    </>
 
   )
 }
